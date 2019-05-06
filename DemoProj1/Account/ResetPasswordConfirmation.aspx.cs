65e205841f11c2888661a40fce2ebfcca2748490
RPSharepoint2019 <prasadaluka0101@gmail.com>
2019-05-06 02:43:54 -0700

    first comment

---
+++ DemoProj1/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DemoProj1.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DemoProj1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoProj1.Startup))]
+namespace DemoProj1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
